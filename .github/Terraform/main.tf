40d79f2c077f7310db324c131c97acbed3d88005
Simen Fossen <simen.fosen@lyse.net>
2025-10-23 11:10:08 +0200

    add IaC terraform for multi env with inputs and fix bootstrap for multi env support

---
+++ .github/Terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.33.0"
+    }
+      random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+    
+  }
+  backend "azurerm" {
+    resource_group_name  = "Homelab-management-RG"
+    storage_account_name = "tfstatehomelab"       # static name
+    container_name       = "tfstate-${var.environment}"       # auto replaced
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+  use_oidc = true
+  skip_provider_registration = true
+}
+
+data "azurerm_client_config" "ServicePrincipal" {
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "homelab-${var.environment}-RG"
+}
